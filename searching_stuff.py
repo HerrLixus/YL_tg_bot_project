4ee799bebd1a8e63b0a3ab178b694cd27d326ebd
HerrLixus <ivanglazkovoff@gmail.com>
2022-04-10 14:12:01 +0300

    probably fixed searching bug

--- searching_stuff.py
+++ searching_stuff.py
@@ -3,7 +3,9 @@ import random
 
 
 def get_random_url(request):
-    response = requests.get(f"https://yandex.ru/images/search?text={request}").text
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                             ' Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52'}
+    response = requests.get(f"https://yandex.ru/images/search?text={request}", headers=headers).text
     data = [x.split('"><img')[0] for x in response.split('<a class="serp-item__link" href="')][1:]
 
     urls = list()
