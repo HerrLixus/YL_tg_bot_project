0acb93121ce1fdf71983244eea48fbab1d0b0e48
HerrLixus <ivanglazkovoff@gmail.com>
2022-04-13 15:24:32 +0300

    now absolutely sure fixed a bug

--- searching_stuff.py
+++ searching_stuff.py
@@ -1,17 +1,20 @@
-import requests
 import random
+import requests
+import utils
+
+
+def search(request):
+    key = utils.read_config()['key']
+    response = requests.get(f'https://www.googleapis.com/customsearch/v1?&key={key}&'
+                            f'cx=21b004bff34927799&searchType=image&q={request}').json()
+    data = response['items']
+    links = [item['link'] for item in data]
+    return links
 
 
 def get_random_url(request):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                             ' Chrome/98.0.4758.109 Safari/537.36 OPR/84.0.4316.52'}
-    response = requests.get(f"https://yandex.ru/images/search?text={request}", headers=headers).text
-    data = [x.split('"><img')[0] for x in response.split('<a class="serp-item__link" href="')][1:]
-
-    urls = list()
-    for index, item in enumerate(list(data)[:10]):
-        url = item.split('img_url=')[1].split('&amp')[0]
-        url = url.replace('%2F', '/')
-        url = url.replace('%3A', ':')
-        urls.append(url)
-    return random.choice(urls) if urls != [] else None
+    try:
+        links = search(request)
+        return random.choice(links) if links != [] else None
+    except Exception:
+        return None
